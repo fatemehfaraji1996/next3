88e640c5357e3e695ff3e497dfc50b98f37f023c
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-08-11 14:12:32 +0330

    rout is ok

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,24 @@
+'use client'
 import Link from "next/link";
 import React from "react";
-
+import { usePathname } from 'next/navigation'
 export default function Nav() {
-  const pages = [{ title: "users",rout:'/users' }, { title: "posts",rout:'/posts' }, { title: "recipes",rout:'/recipes' }];
+  const pages = [
+    { title: "users", rout: "/users" },
+    { title: "posts", rout: "/postes" },
+    { title: "recipes", rout: "/recipes" },
+  ];
+  const path = usePathname()
   return (
-    <div className='flex gap-14 bg-green-300 p-8'>
-       {pages.map((item, i) => {
-        return(
 
-<Link className='' href={item.rout}>{item.title}</Link>
-
-        ) 
-      })} 
-      
+    <div className="flex gap-14 bg-green-300 p-8">
+      {pages.map((item, i) => {
+        return (
+          <Link className="" href={item.rout}>
+            {item.title}
+          </Link>
+        );
+      })}
     </div>
   );
 }
