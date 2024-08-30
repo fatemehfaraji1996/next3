5d265e1ae2fc2e80dfb7a0533b4b7d1e9491a9ff
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-08-30 23:39:26 +0330

    commit

--- src/app/api/admin/posts/[postid]/route.js
+++ src/app/api/admin/posts/[postid]/route.js
@@ -1,5 +1,7 @@
 import GetData from "@/utils/fetchData";
 
+
+
 export async function GET(req, { params }) {
   const data = await GetData("https://dummyjson.com/posts");
   const recipe = data.posts;
