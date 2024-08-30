dc573a6ed252bb947efb3391d75e1b8cfa7249af
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-08-31 00:11:38 +0330

    post ok

--- src/app/api/admin/posts/route.js
+++ src/app/api/admin/posts/route.js
@@ -3,21 +3,21 @@
 // }
 
 import GetData from "@/utils/fetchData";
-const arr = [{
-  0: 'a',
-  1: 'b',
-  2: 'c'
-}]
+const arr = [
+  {
+    0: "a",
+    1: "b",
+    2: "c",
+  },
+];
 export async function GET() {
   const data = await GetData("https://dummyjson.com/posts");
   const user = data.posts;
   return Response.json(data);
 }
 
-
 export async function POST(req) {
-const body = await req.json()
-arr.push(body)
-  return Response.json(arr)
-
+  const body = await req.json();
+  arr.push(body);
+  return Response.json(arr);
 }
