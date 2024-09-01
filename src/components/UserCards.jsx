f860f9a9350c6e708a502be88e6f529dc2770368
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-09-01 11:15:45 +0330

    commit

--- src/components/UserCards.jsx
+++ src/components/UserCards.jsx
@@ -8,14 +8,15 @@ export default async function UserCards() {
 
   return (
     <>
-      <button className='bg-lime-400 p-4 '>Add User</button>
+      <button id='addb' className='bg-lime-400 p-4 '>Add User</button>
       <input className='bg-lime-200 p-4' type="text" />
 
 
-    <div className="flex flex-wrap gap-14 bg-green-100 mt-9 p-10 ">
+
+    <div className="flex flex-wrap gap-10 bg-green-100 mt-9 p-10 ">
       {dataOfUsers.map((x) => (
         <Link href={`/users/${x.id}`}>
-          <button className=" border-2 p-5 border-lime-300">
+          <button className=" border-2 p-4 border-lime-300">
             {x.username}
           </button>
         </Link>
