5cefa55c9bb36e824af78753f882688c8177e104
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-09-06 17:34:12 +0330

    handel form

--- src/components/CreatPost.jsx
+++ src/components/CreatPost.jsx
@@ -1,32 +1,28 @@
 "use client";
+import { addUser } from "@/utils/actions";
 import React from "react";
 import { useState } from "react";
 
 export default function CreatPost() {
-  const [title, settitle] = useState();
-  async function handlsubmit(e) {
+  const [title, setTitle] = useState();
+  async function handleSubmit(e) {
     e.preventDefault();
-    settitle("");
+    await addUser(title);
+    setTitle("");
   }
 
   return (
-    <form onSubmit={(e) => handlsubmit()}>
+    <form onSubmit={(e) => handleSubmit(e)}>
       <div className="m-5 bg-lime-200 w-72">
         <p className="m-10 italic  ">creat new post</p>
         <p>post</p>
-        <input
-          value={title}
-          placeholder="Enter your post"
-          onChange={(e) => settitle(e.target.value)}
-          className="bg-lime-300 p-4 m-2"
-          type="text"
-        />
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
         <p> caption</p>
-        <input
+        {/* <input
           placeholder="Enter your caption"
           className="bg-lime-300 p-4 m-2"
           type="text"
-        />
+        /> */}
         <button type="submit" className="bg-lime-400 p-3 ">
           Add Post
         </button>
--- src/utils/actions.js
+++ src/utils/actions.js
@@ -1,3 +1,3 @@
-async function addRace(reacTtitle) {
+export async function addUser(reacTtitle) {
     fetch('https://dummyjson.com/posts',{method: "POST", body: JSON.stringify({ username: "example" })})
 }
