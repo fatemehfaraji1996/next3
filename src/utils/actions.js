c2f1fd0fe9575e0569076b627eda4537a6991d96
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-09-09 09:46:18 +0330

    commit

--- src/utils/actions.js
+++ src/utils/actions.js
@@ -1,3 +1,4 @@
 export async function addUser(reacTtitle) {
-    fetch('https://dummyjson.com/posts',{method: "POST", body: JSON.stringify({ username: "example" })})
+    fetch('https://dummyjson.com/posts',
+        {method: "POST", body: JSON.stringify({ username: "example" })})
 }
