137e9fc67b05e27c6360a8dd30a72616a6f2c8a6
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-09-14 22:10:54 +0330

    commit8

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,32 +1,25 @@
-'use client'
+"use client";
 import Link from "next/link";
 import React from "react";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 export default function Nav() {
   const pages = [
     { title: "users", rout: "/users" },
     { title: "posts", rout: "/postes" },
     { title: "recipes", rout: "/recipes" },
   ];
-  const path = usePathname()
+  const path = usePathname();
   return (
-
     <div className="flex gap-14 bg-green-300 p-8">
       {pages.map((item, i) => {
         return (
-
           <>
-          
-          <Link key={i} className="" href={item.rout}>
-            {item.title} 
-          </Link>
-        
+            <Link key={i} className="" href={item.rout}>
+              {item.title}
+            </Link>
           </>
-          
         );
       })}
     </div>
-
-
   );
 }
