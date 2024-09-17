7a144a178665d4cf93e99bcc3d6aba24b09f8eb8
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-09-17 23:41:56 +0330

    commit

--- src/components/PostCards.jsx
+++ src/components/PostCards.jsx
@@ -6,7 +6,7 @@ import CreatPost from "./CreatPost";
 // import CreatePost from "./CreatePost";
 
 export default async function PostCards() {
-  const posts = await GetData("http://localhost:3000/api/admin/posts",{
+  const posts = await GetData("http://localhost:3000/api/admin/posts", {
     next: { tags: ["recatch"] },
   });
 
@@ -14,15 +14,16 @@ export default async function PostCards() {
     <>
       <CreatPost></CreatPost>
       {posts.map((x) => (
-        <Link key={x.id} href={`/posts/${x.id}`}>
-          <div className="flex w-2/3 bg-lime-200 p-10">
+        <div className="flex w-2/3 bg-lime-200 p-10">
+          <Link key={x.id} href={`/posts/${x.id}`}>
             <div className="border-8 p-5">
               <p className="p-5">{x.title}</p>
               <br />
               <p>{x.body}</p>
             </div>
-          </div>
-        </Link>
+          </Link>
+          <button className="bg-red-600 w-28 p-4 m-10 h-20">Delet Post</button>
+        </div>
       ))}
     </>
   );
