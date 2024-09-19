970410ab0f0a94b84b2b127593b9db10cfe24c79
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-09-19 19:37:40 +0330

    ncommit

--- src/app/api/admin/posts/[postid]/route.js
+++ src/app/api/admin/posts/[postid]/route.js
@@ -9,30 +9,10 @@ export async function GET(req, { params }) {
   //   return Response.json("erore");
   // }
   // return Response.json(response);
-  
-  const post = data.find((x) => x.id == parseInt(params.postid));
+
+  const post = data.find((x) => x.id === parseInt(params.postid));
   // if (!response) {
   //   return Response.json("erore");
   // }
   return Response.json(post);
 }
-
-
-
-// import GetData from "@/utils/fetchData";
-
-// export async function GET(req, { params }) {
-//  try {
-//  const data = await GetData(`http://localhost:3000/api/admin/posts/${params.postid}`);
-
-//  const post = data.find((x) => x.id === parseInt(params.postid));
-
-//  if (!post) {
-//   return Response.json("error", { status: 404 });
-//  }
-//  return Response.json(post);
-//  } catch (error) {
-//  console.error('Error fetching data:', error);
-//  return Response.json("Internal Server Error", { status: 500 });
-//  }
-// }
