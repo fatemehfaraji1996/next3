26da4de1a36d18c1417abeb7103801924f7102b8
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-09-19 20:40:25 +0330

    commit

--- src/app/api/admin/posts/[postid]/route.js
+++ src/app/api/admin/posts/[postid]/route.js
@@ -1,8 +1,6 @@
-import GetData from "@/utils/fetchData";
 import { data } from "../route";
 export async function GET(req, { params }) {
-
   const post = data.find((x) => x.id === parseInt(params.postid));
- 
+
   return Response.json(post);
 }
